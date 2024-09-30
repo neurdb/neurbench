1ecb6369a0e38ae46f29d5f6a1ffe688c6e5ed3e
working <57171759+NLGithubWP@users.noreply.github.com>
2024-09-30 16:48:05 +0800

    Add some comments

--- neurbench/drift.py
+++ neurbench/drift.py
@@ -6,6 +6,7 @@ from .dist import zipf_dist
 
 
 def _objective(q, p, alpha):
+    # alphaL: target JS divergence between original dist p and target dist q
     return (jensenshannon(p, q) - alpha) ** 2
 
 
@@ -14,13 +15,17 @@ def _constraint(q):
 
 
 def find_q(p: List[float], d: float, skewed: bool = True) -> List[float]:
+    # find a distribution q which is as close as p (measured by JS divergence)
     n = len(p)
     if skewed:
+        # initial guess for the distribution
         q0 = zipf_dist(n, 1.0)
     else:
         q0 = np.ones(n) / n
 
+    # sum of probabilities must equal 1
     cons = {"type": "eq", "fun": _constraint}
+    # each pribability is from 0 to 1
     bounds = [(0, 1) for _ in range(n)]
 
     result = minimize(
